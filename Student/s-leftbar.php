8e2d07460abcdeca24f72a65659a0537549ee8dc
ashcharyadasanayake <aishadasana@gmail.com>
2024-02-13 11:00:11 +0530

    student left bar

---
+++ Student/s-leftbar.php
@@ -0,0 +1,73 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
+                                <h6 class="title">John Doe</h6>
+                                <small class="info">PHP Developer</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="s-dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Profile</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>View Profile</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Manage Profile</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+
+                                   
+ <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Videos</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+
+                                            <li><a href="view-Video.php"><i class="fa fa-newspaper-o"></i> <span>View video</span></a></li>
+                                           
+                                           
+                                        </ul>
+                                    </li>
+                                    
+   
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Result</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="find-result.php"><i class="fa fa-bars"></i> <span>View Result</span></a></li>
+                                            
+                                        </ul>
+                                        
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>get in touch</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+
+                                            <li><a href="get in touch.php"><i class="fa fa-newspaper-o"></i> <span>get in touch</span></a></li>
+                                           
+                                           
+                                        </ul>
+                                    </li> 
+ <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>change password</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+
+                                            <li><a href="s-change-password.php"><i class="fa fa-newspaper-o"></i> <span>change password</span></a></li>
+                                           
+                                           
+                                        </ul>
+                                    </li>                                                                                          
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
