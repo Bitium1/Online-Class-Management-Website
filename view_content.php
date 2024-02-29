11143188ab87a8b0326d3c62d9671b3453501d60
rashmika3433 <65844409+rashmika3433@users.noreply.github.com>
2024-02-29 10:25:32 +0530

    bug fixed

---
+++ view_content.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+include('includes/config.php');
+
+if(strlen($_SESSION['alogin']) == "") {   
+    header("Location: index.php"); 
+    exit(); // Add exit() after header to stop further execution
+} else {
+    
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <title>View Video</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="css/admin_style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
+    <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
+    <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen" >
+    <link rel="stylesheet" href="css/prism/prism.css" media="screen" >
+    <link rel="stylesheet" href="css/select2/select2.min.css" >
+    <link rel="stylesheet" href="css/main.css" media="screen" >
+</head>
+<body class="top-navbar-fixed">
+    <div class="main-wrapper">
+        <!-- ========== TOP NAVBAR ========== -->
+        <?php include('includes/topbar.php');?> 
+
+        <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
+        <div class="content-wrapper">
+            <div class="content-container">
+                <!-- ========== LEFT SIDEBAR ========== -->
+                <?php include('includes/leftbar.php');?>  
+
+                <div class="main-page">
+                    <div class="container-fluid">
+                        <!-- Your container fluid content here -->
+                    </div>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="panel">
+                                    <div class="panel-heading">
+                                        <div class="panel-title">
+                                            <h3>View Video</h3>
+                                        </div>
+                                        
+                                        <div class="panel-body">
+    <div class="row">
+        
+    </div>
+</div>
+
+</div>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/pace/pace.min.js"></script>
+    <script src="js/lobipanel/lobipanel.min.js"></script>
+    <script src="js/iscroll/iscroll.js"></script>
+    <script src="js/prism/prism.js"></script>
+    <script src="js/select2/select2.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
